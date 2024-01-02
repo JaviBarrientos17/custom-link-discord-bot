820fbc6066fc40619115f3f3d93778a40a11ce16
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2024-01-02 22:26:34 +0100

    Testing version

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const { Client } = require("discord.js");
+const { Client, Permissions } = require("discord.js");
 const client = new Client();
 
-client.on("message", (message) => {
-  if (message.content === "!saludo") {
-    message.reply("¡Hola!");
-  } else if (message.content === "!despedida") {
-    message.reply("¡Hasta luego!");
-  }
+client.on("ready", () => {
+  console.log(`Bot is ready!`);
+
+  // Obtén el canal donde deseas establecer permisos
+  const channel = client.channels.cache.get("1191850645343453254"); // Reemplaza 'ID_DEL_CANAL' con el ID real del canal
+
+  // Establece permisos para el bot en ese canal
+  channel.createOverwrite("1191852386956869683", {
+    VIEW_CHANNEL: true,
+    SEND_MESSAGES: true,
+  });
 });
 
-// Obtén el token desde la variable de entorno
-client.login(process.env.BOT_TOKEN);
+client.login("MTE5MTg1MjM4Njk1Njg2OTY4Mw.GHISVY.-uTGVHJ5rj2u0akJiAyytMORAUnawB4uhk_vxQ");
